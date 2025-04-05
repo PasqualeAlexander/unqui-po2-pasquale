f383ba5bc172da72429cd662c2eca41f743e8795
Pasquale, Alexander <142181066+PasqualeAlexander@users.noreply.github.com>
2025-04-05 13:13:31 -0300

    Update Counter.java

--- Counter.java
+++ Counter.java
@@ -1,6 +1,37 @@
 package ar.edu.unq.po2.tp3;
+import java.util.ArrayList;
 
 public class Counter {
-    // Class implementation will go here
+	
+	private ArrayList<Integer> numeros = new ArrayList<Integer>();
+	
+	public int cantidadDeNumerosPares() {
+		int contador = 0;
+		for (int num : numeros) {
+			if (num % 2 == 0) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+	
+	public int cantidadDeNumerosImpares() {
+		int contador = 0;
+		for (int num : numeros) {
+			if (num % 2 != 0) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+	
+	public int contarMultiplosDe(int numero) {
+        int contador = 0;
+        for (int num : numeros) {
+            if (num % numero == 0) {
+                contador++;
+            }
+        }
+        return contador;
+    }
 }
-
