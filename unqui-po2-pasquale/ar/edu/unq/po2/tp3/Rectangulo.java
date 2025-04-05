11c50de57a7dd7aca92fc3ac672a7f38657e93a1
PasqualeAlexander <alexanderpasquale1@gmail.com>
2025-04-05 13:57:50 -0300

    Agregado Rectangulo.java al tp3

---
+++ unqui-po2-pasquale/ar/edu/unq/po2/tp3/Rectangulo.java
@@ -0,0 +1,31 @@
+package ar.edu.unq.po2.tp3.rectangulo;
+
+public class rectangulo {
+
+public class Punto {
+	int x;
+	int y;
+
+	public int getX() {
+		return x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public Punto(int x, int y) {
+		super();
+		this.x= x;
+		this.y = y;
+	}
+}
+}
