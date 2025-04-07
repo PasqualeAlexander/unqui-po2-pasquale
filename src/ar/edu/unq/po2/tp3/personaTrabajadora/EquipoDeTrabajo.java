58e970dbfe58071a489992b8cdc65b6bf6cc7ed7
PasqualeAlexander <alexanderpasquale1@gmail.com>
2025-04-07 19:28:04 -0300

    Agrego los ejercicios persona y EquipoDeTarabajo

---
+++ src/ar/edu/unq/po2/tp3/personaTrabajadora/EquipoDeTrabajo.java
@@ -0,0 +1,37 @@
+package ar.edu.unq.po2.tp3.personaTrabajadora;
+
+import java.util.ArrayList;
+
+public class EquipoDeTrabajo {
+    private String nombreDelEquipo;
+    private ArrayList<PersonaTrabajadora> integrantes;
+    
+    public EquipoDeTrabajo(String nombreDelEquipo) {
+        this.nombreDelEquipo = nombreDelEquipo;
+        this.integrantes = new ArrayList<>();
+    }
+
+    public String getNombreDelEquipo() {
+        return nombreDelEquipo;
+    }
+    
+    public void agregarIntegrante(PersonaTrabajadora persona) {
+        integrantes.add(persona);
+    }
+    
+    private int sumarEdadesIntegrantes() {
+        int total = 0;
+        for (PersonaTrabajadora persona : integrantes) {
+            total += persona.getEdad();
+        }
+        return total;
+    }
+    
+    public double promedioEdadIntegrantes() {
+        if (integrantes.isEmpty()) {
+            return 0;
+        }
+        return sumarEdadesIntegrantes() / integrantes.size();
+    }
+}
+
